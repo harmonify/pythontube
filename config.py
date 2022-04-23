d058f2a00c5211808a7f72524c340a8229948010
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-04-23 14:07:57 +0700

    add `pythontube` and the config script

---
+++ config.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+import json
+import os
+
+
+class Config:
+    """
+    The config class for python-script.
+
+    Attributes:
+    - name: the config file name (default: python-script)
+    - __location__: the path of the config script
+    - __location__: the path of the project root
+    - file_path: the config file path (defaulting to `name` attribute in the
+                 project root directory)
+    - data: the data of the config file
+
+    Methods:
+    - read_config(): read the config file and return the config dict
+    - initialize_config(): initialize the config file and return the config dict
+    """
+
+    def __init__(self, file_name: str = "python-script") -> None:
+        self.__location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self.file_name = file_name
+        # file_path: the path of the config file (defaulting to python-script.json
+        # in the project root directory)
+        self.file_path = os.path.join(
+            self.__location__, self.file_name).removesuffix(".json") + ".json"
+        self.data = self.read_config()
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} name={self.file_name}>"
+
+    def read_config(self) -> dict[str, str]:
+        """ Read the config file and return the data. """
+        try:
+            print(f"Reading {self.file_path}")
+            if os.path.isdir(self.file_path):
+                raise FileNotFoundError(
+                    f"{self.file_path} is a directory, not a file")
+            elif os.path.isfile(self.file_path):
+                print(f"{self.file_path} is a file")
+                with open(self.file_path, "r") as f:
+                    data = json.loads(f.read())
+            else:
+                print(f"{self.file_path} does not exist")
+                data = self.initialize_config()
+            print("Done")
+            return data
+        except FileNotFoundError as e:
+            print(e)
+            exit(1)
+        except json.JSONDecodeError:
+            print(f"{self.file_path} is not a valid JSON file")
+            exit(1)
+
+    def initialize_config(self) -> dict[str, str]:
+        """ Initialize the config file and return the data. """
+        print(f"Initializing {self.file_name}")
+        # constants
+        data = {
+            "app": self.file_name,
+            "version": "1.0",
+        }
+
+        # user input
+        data["output_dir_path"] = self._input_output_dir_path()
+
+        # write config
+        with open(self.file_path, "w") as f:
+            f.write(json.dumps(data))
+        print(f"{self.file_path} is created")
+        return data
+
+    def _input_output_dir_path(self) -> str:
+        """
+        Input the output directory path.
+
+        Returns:
+        The output directory path.
+        """
+        while True:
+            print(
+                "\nEnter the output directory path (e.g. /home/user/python-script-output)")
+            print("Default: '<script-location>/python-script-output/'")
+            output_dir = input(": ") or os.path.join(
+                self.__location__, "python-script-output")
+
+            # check if user specify a relative path or absolute path
+            if os.path.isabs(output_dir):
+                output_dir_path = os.path.realpath(output_dir)
+            else:
+                output_dir_path = os.path.realpath(
+                    os.path.join(os.getcwd(), *output_dir.split("/")))
+
+            if not os.path.isdir(output_dir_path):
+                print(f"{output_dir_path} is not a directory")
+                if input("Create directory (y/n)? ") == "y":
+                    os.makedirs(output_dir_path)
+                    print(f"{output_dir_path} is created")
+                    break
+            else:
+                print(f"\n{output_dir_path} is a directory")
+                if input("Is this the right path (y/n)? ") == "y":
+                    break
+        return output_dir_path
+
+
+def main(args=None):
+    try:
+        config = Config("python-script")
+        print("==========================")
+        print(config.initialize_config())
+        print("==========================\n")
+
+        print("==========================")
+        print(config.read_config())
+        print("==========================\n")
+    except KeyboardInterrupt:
+        print("Exiting...")
+
+
+if __name__ == '__main__':
+    main()
